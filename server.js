bd46c4327ac23674a3c397f7f672c5e4eb6930ad
Jorge Castello <60675751+jorgio-castello@users.noreply.github.com>
2020-09-19 11:32:53 -0500

    Add additional route

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cors());
 
-
+app.get('/whatUp', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/whatUp.html'));
+});
 
 app.listen(3000, () => console.log('I am working'));
